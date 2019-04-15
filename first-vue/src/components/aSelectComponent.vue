df4cf09697fa2a42fe3259788468a33dbd28c1ed
Lewinn <1289054631@qq.com>
2019-04-15 18:42:45 +0800

    add components

---
+++ first-vue/src/components/aSelectComponent.vue
@@ -0,0 +1,18 @@
+<template>
+	<select v-model='selected'  @change="setValue">
+		<option value='aaa'>AAA</option>
+		<option :value='op'>{{op}}</option>
+	</select>
+</template>
+
+<script>
+	export default {
+		name: 'aSelect',
+		props: ['op'],
+		methods: {
+			setValue: function(){
+				this.$emit('selectedValue',this.selected)
+			}
+		}
+	}
+</script>
--- first-vue/src/components/home.vue
+++ first-vue/src/components/home.vue
@@ -1,13 +1,28 @@
 <template>
+	<div>
 	<h2>Hello {{ name }}, here is HOME page.</h2>
+
+	<Selecttt :op="opt" @selectedValue="getValue"></Selecttt>
+	<h3><b>The selected value is:</b><em>{{ selectedValue }}</em></h3>
+	</div>
 </template>
 
 <script>
+	import Selecttt from '@/components/aSelectComponent'
+
 	export default {
 		name: 'home',
+		components: {Selecttt},
 		data(){
 			return {
-				name: "Lewin<huanglewin@gmail.com>"
+				name: "Lewin<huanglewin@gmail.com>",
+				opt: "firstOption",
+				selectedValue: ''
+			}
+		},
+		methods: {
+			getValue(msg){
+				this.selectedValue = msg;
 			}
 		}
 	}
